5ba93e66f89be8e252f3fcb05ee3a6b5b915bc10
maxmyth01 <maxmyth01@gmail.com>
2017-08-31 14:28:14 -0400

    Updated from Brython Server: 8/31/2017 2:28:13 PM

--- face.py
+++ face.py
@@ -1,1 +1,11 @@
+#Max Low
+#8-31-17
+#face.py - make a creepy face
+
+print('   XXXXX   ')
+print('  X     X  ')
+print('((  o o  ))')
+print('  |  V  |  ')
+print('  | === |  ')
+print('   -----   ')
 
