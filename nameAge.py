35709f83e41c85fe95dcfa690724f89e84bf12fb
maxmyth01 <maxmyth01@gmail.com>
2017-08-31 14:51:26 -0400

    Updated from Brython Server: 8/31/2017 2:51:25 PM

--- nameAge.py
+++ nameAge.py
@@ -1,1 +1,14 @@
+#Max Low
+#8-31-17
+#nameAge.py - identifies the amount of charactures
 
+Name = input('What is your name?(first and last)')
+Age = int(input('What is your age?'))
+
+Name1,Name2 = Name.split()
+
+print('Your first name has', len(Name1) ,'letters')
+
+print('Your last name has', len(Name2) ,'letters')
+
+print('You will be', Age+1 ,'years old next year')
