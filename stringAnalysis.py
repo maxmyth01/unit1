f0e46b802c5f3203eed38172b9668be050512340
maxmyth01 <maxmyth01@gmail.com>
2017-09-01 14:53:34 -0400

    Updated from Brython Server: 9/1/2017 2:53:33 PM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -1,1 +1,14 @@
+#Max Low
+#9-1-17
+#stringAnalysis.py - Studies a sentance and counts the amount of words letter and scans for a letter and word
+
+sentance = input('Enter a Sentance')
+Print('Your sentance has', ,'words and', ,'characters and', ,'letters')
+
+letter = input('Enter a character to search for:')
+print('Your sentance has', ,'of the character',letter)
+
+word = input('Enter a word to search for:')
+print('Your sentance has', ,'of the word',word')
+
 
