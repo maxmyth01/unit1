168be6c7ea7e15d4174bb9705523f4fd2866e7cd
maxmyth01 <maxmyth01@gmail.com>
2017-09-01 14:40:00 -0400

    Update slope.py

--- slope.py
+++ slope.py
@@ -3,6 +3,14 @@
 #slope.py - calulator of the equation of a line given two points
 
 x1 = int(input('x1'))
-x1 = int(input('x1'))
-x1 = int(input('x1'))
-x1 = int(input('x1'))
+y1 = int(input('y1'))
+x2 = int(input('x2'))
+y2 = int(input('y2'))
+
+slope = (y1-y2)/(x1-x2)
+
+print('The slope is',slope)
+
+b=(y1-(slope*x1))
+
+print('The equation of the line is Y =',slope,'X+',b)
