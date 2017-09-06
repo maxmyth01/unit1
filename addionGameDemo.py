7c800ac95b125bfcb5d9e27c2424bd3cfdf2243a
maxmyth01 <maxmyth01@gmail.com>
2017-09-06 13:33:01 -0400

    Updated from Brython Server: 9/6/2017 1:33:00 PM

--- addionGameDemo.py
+++ addionGameDemo.py
@@ -1,1 +1,11 @@
+#Max Low
+#9-6-17
+#addationGameDemo.py - our first game
+
+from random import randint
+
+num1 =randint(1,10)
+num2 =randint(1,10)
+answer=int(input(str(num1)+'+'+str(num2)+'='))
+print(answer == num1+num2)
 
