02e24129e400b907e35f07d5601cbebb66a15016
maxmyth01 <maxmyth01@gmail.com>
2017-09-07 14:47:17 -0400

    Updated from Brython Server: 9/7/2017 2:47:16 PM

--- ItATriangle.py
+++ ItATriangle.py
@@ -1,1 +1,13 @@
+#Max Low
+#9-7-17
+#ItATriangle.py - can you make a triangle
+
+A = float(input('Side A'))
+B = float(input('Side B'))
+C = float(input('Side C'))
+
+print(A+B+C-max(A,B,C) - min(A,B,C) >= A+B+C-max(A,B,C))
+
+
+
 
