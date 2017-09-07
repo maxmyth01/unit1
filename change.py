8d12da37bb92f01a90439cf06d9ca585d39409f3
maxmyth01 <maxmyth01@gmail.com>
2017-09-07 14:37:55 -0400

    Updated from Brython Server: 9/7/2017 2:37:55 PM

--- change.py
+++ change.py
@@ -1,3 +1,14 @@
 #Max Low
 #9-7-17
 #change.py - enter an amount of cents and converts into coins
+
+change = float(input('Enter an amount of change')) 
+change = change*100
+quarter = change//25
+change = change - 25*quarter
+dime = change//10
+change = change - 10*dime
+nickle =change//5
+change = change - 5*nickle
+pennie = change
+print('That is',quarter ,'Quarters', dime ,'Dimes', nickle ,'Nickles',pennie,'Pennies')
