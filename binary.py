de28546b8396cefea82544d9d2fc1ea7d9ba28ac
maxmyth01 <maxmyth01@gmail.com>
2017-09-08 14:42:43 -0400

    Updated from Brython Server: 9/8/2017 2:42:42 PM

--- binary.py
+++ binary.py
@@ -2,5 +2,30 @@
 #9-7-17
 #Binary.py - binary to base 10 converter
 
-int(input('Input and 8 digit binary number'))
+binary = int(input('Input and 8 digit binary number'))
 
+runningTotal = (binary%10)
+binary = (binary//10)
+
+runningTotal = runningTotal+ 2**(1*(binary%10))
+binary = (binary//10)
+
+runningTotal = runningTotal+ 2**(2*(binary%10))
+binary = (binary//10)
+
+runningTotal = runningTotal+ 2**(3*(binary%10))
+binary = (binary//10)
+
+runningTotal = runningTotal+ 2**(4*(binary%10))
+binary = (binary//10)
+
+runningTotal = runningTotal+ 2**(5*(binary%10))
+binary = (binary//10)
+
+runningTotal = runningTotal+ 2**(6*(binary%10))
+binary = (binary//10)
+
+runningTotal = runningTotal+ 2**(7*(binary%10))
+binary = (binary//10)
+
+print('That is',runningTotal)
