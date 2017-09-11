c5a029feed0d90bce69cf8812ba71e5d7c5141f5
maxmyth01 <maxmyth01@gmail.com>
2017-09-11 14:24:06 -0400

    Updated from Brython Server: 9/11/2017 2:24:04 PM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,16 @@
+#Max Low
+#9-11-17
+#quiz1.py - print name, enter numbers, do math and print, print a lucky number
+
+from random import randint
+
+print('Max Low')
+
+num1 = float(Input('Enter a number: '))
+
+num2 = float(Input('Enter an another number: '))
+
+print('The sum is',num1+num2)
+print('The product is',num1*num2)
+print('Your lucky number is',randint(-10,10))
 
